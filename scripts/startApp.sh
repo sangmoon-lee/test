91134c7e3d890560f1e9749c879a98ec812689ac
sujin-jung <sjjung@lgcns.com>
2019-07-25 17:21:48 +0900

    test

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -2,5 +2,5 @@
 echo "starting springboot application...."
 SVC_NAME='app_service'
 
-sudo ln -sf /home/ec2-user/deploy/*.war /etc/init.d/$SVC_NAME
+sudo ln -sf /home/ec2-user/deploy/*.jar /etc/init.d/$SVC_NAME
 sudo service $SVC_NAME start
